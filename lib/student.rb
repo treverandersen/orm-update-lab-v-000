cb9e5761dfd8880e98729a147473ff11de1aa2bc
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 23:41:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,4 +43,9 @@ class Student
     end
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 end
