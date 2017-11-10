9e3d0b634d8f0f933ce566e896fa49fa3d685350
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-10 23:33:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,10 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(id=nil, name, grade)
-    @id = id 
+    @id = id
     @name = name
     @grade = grade
   end
