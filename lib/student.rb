2bdbc874a22f769cb9194e53d6c144a7b68feec0
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-11 00:36:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,7 @@ class Student
       WHERE name = ?
       LIMIT 1
     SQL
-    DB[:conn].excute(sql, name).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
